b276b7445969efe108012292c67860e452baa61a
Joe <j.joe.douglas@gmail.com>
2017-02-18 14:51:37 -0500

    iterateWorld

--- src/World.hs
+++ src/World.hs
@@ -1,18 +1,32 @@
+<<<<<<< HEAD
 module World (World, getWorld, buildWorld, getNeighbors, lookupCell) where
 
 import Cell
 import Position
+=======
+module World (World, getWorld, deadWorld, buildWorld, getNeighbors, lookupCell, iterateWorld) where
+
+import Cell
+import Game
+>>>>>>> iterateWorld
 
 newtype World = W {getWorld :: [[Cell]]}
 
 instance Show World where
   show (W cells) = cells >>= (\row -> show row ++ "\n")
+instance Eq World where
+  a == b = (getWorld a) == (getWorld b)
 
 buildWorld :: Int -> Int -> [Position] -> World
 buildWorld col row liveCells = W [
   [ if mkPosition r c `elem` liveCells then Live else Dead
     | r <- [0..(row-1)]]
     | c <- [0..(col-1)]]
+
+iterateWorld :: World -> World
+iterateWorld world =
+  let w = (flip fmap) (getWorld world `zip` [(0 :: Int)..]) (\x -> fmap (zipWith (\y -> \z -> ((snd x,y), z)) [(0 :: Int)..]) fst x)
+  in W (fmap (fmap (\x -> newState (snd x) (getNeighbors world ((uncurry mkPosition) fst x)))) w)
 
 getNeighbors :: World -> Position -> [Cell]
 getNeighbors world position = let
