5177c3fbfad12ec66873c3948521f3432bac03ee
mac10688 <mac10688@protonmail.com>
2017-03-19 20:16:20 -0700

    reactive-banana implemented

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,32 @@
 module Main where
 
+import System.Console.ANSI
+import Control.Concurrent
+
+import World (World, buildWorld, iterateWorld)
+import Position (Position, mkPosition)
+
+import Reactive.Banana
+import Reactive.Banana.Frameworks
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  (addHandler, trigger) <- newAddHandler 
+  network <- setupNetwork addHandler
+  actuate network
+  eventLoop trigger
+
+eventLoop :: (() -> IO()) -> IO ()
+eventLoop trigger = loop
+  where
+  loop = do
+    trigger ()
+    threadDelay 1000000
+    loop
+
+setupNetwork :: AddHandler () -> IO EventNetwork
+setupNetwork addHandler = compile $ do
+  etick <- fromAddHandler addHandler
+  let startWorld = buildWorld 5 5 [mkPosition 2 2, mkPosition 1 2, mkPosition 3 2]
+  eWorld <- accumE startWorld $ iterateWorld <$ etick
+  reactimate $ fmap (\world -> clearScreen >> print world) eWorld
