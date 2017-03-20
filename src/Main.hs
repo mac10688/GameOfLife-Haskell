1b7925239e076d5792bf63fd7014b278457479c2
mac10688 <mac10688@protonmail.com>
2017-03-19 21:57:15 -0700

    Adding comments for reactive banana

--- src/Main.hs
+++ src/Main.hs
@@ -11,8 +11,14 @@ import Reactive.Banana.Frameworks
 
 main :: IO ()
 main = do
+ --Create an add handler and the trigger
+ --The add handler will be wired into the network
+ --The trigger is a function that just needs one argument
+ --When it recieves the argument the addHandler will be fired
   (addHandler, trigger) <- newAddHandler 
   network <- setupNetwork addHandler
+  --start the network.
+  --Have the listeners listen for the function to be fired
   actuate network
   eventLoop trigger
 
@@ -20,13 +26,22 @@ eventLoop :: (() -> IO()) -> IO ()
 eventLoop trigger = loop
   where
   loop = do
+    --fire the function
     trigger ()
+    --wait 1 second
     threadDelay 1000000
     loop
 
 setupNetwork :: AddHandler () -> IO EventNetwork
 setupNetwork addHandler = compile $ do
+  --Get the event. I really don't know how to explain this.
   etick <- fromAddHandler addHandler
+  --Creates an initial world to start
   let startWorld = buildWorld 5 5 [mkPosition 2 2, mkPosition 1 2, mkPosition 3 2]
+  --accumE will take an initial world.
+  --The next part will be an Event function that will iterate
+  --to the next world for each event fired.
   eWorld <- accumE startWorld $ iterateWorld <$ etick
+  --Each time there is an eWorld event, print it.
+  --That's what reactimate does.
   reactimate $ fmap (\world -> clearScreen >> print world) eWorld
