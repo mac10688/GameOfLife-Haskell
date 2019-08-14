7d64641bc69af8826a47fb2388bf37dfcad851cd
mac10688 <mac10688@gmail.com>
2019-08-13 22:37:34 -0700

    Adding cabal2nix bash file

---
+++ projext.nix
@@ -0,0 +1,15 @@
+{ mkDerivation, ansi-terminal, base, hspec, reactive-banana, stdenv
+}:
+mkDerivation {
+  pname = "GameOfLife-Haskell";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = true;
+  isExecutable = true;
+  libraryHaskellDepends = [ base ];
+  executableHaskellDepends = [ ansi-terminal base reactive-banana ];
+  testHaskellDepends = [ base hspec ];
+  homepage = "https://github.com/githubuser/GameOfLife-Haskell#readme";
+  description = "Simple project template from stack";
+  license = stdenv.lib.licenses.bsd3;
+}
